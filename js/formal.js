972282c7c3c5852ae1aeb570892c265ba2f8c0bb
Yue OuYang <2925886492@qq.com>
2023-12-12 18:28:15 +0800

    Site updated: 2023-12-12 18:28:14

--- js/formal.js
+++ js/formal.js
@@ -1,10 +1,10 @@
-if(localStorage.getItem("blogbg")!=undefined){
+if(localStorage.getItem("blogbg") !== null){
     setBg(localStorage.getItem("blogbg"));
 }else{
     document.getElementById("defineBg").innerText = `:root{
         --default-bg: url(https://i.postimg.cc/8cTtV7Xt/20231212164342.jpg);
         --darkmode-bg: url(https://i.postimg.cc/8cTtV7Xt/20231212164342.jpg);
         --mobileday-bg: url(https://i.postimg.cc/8cTtV7Xt/20231212164342.jpg);
-        --mobilenight-bg: url(https://i.postimg.cc/8cTtV7Xt/20231212164342.jpg)'
+        --mobilenight-bg: url(https://i.postimg.cc/8cTtV7Xt/20231212164342.jpg);
     }`
 }
